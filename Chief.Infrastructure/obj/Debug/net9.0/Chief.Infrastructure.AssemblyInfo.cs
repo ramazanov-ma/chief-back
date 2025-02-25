fb29f00389eadc6bab856b30b7e80815589ab884
Ramazanov Marat <maratoff20@gmail.com>
2025-02-25 19:53:00 +0300

    Update gitignore

--- Chief.Infrastructure/obj/Debug/net9.0/Chief.Infrastructure.AssemblyInfo.cs
+++ Chief.Infrastructure/obj/Debug/net9.0/Chief.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Chief.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cb9d1206204edc7ab82a57fb82568d5e1ffb3d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7a3bbcf56df6cbb8543b02923dd8eacf117bba7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Chief.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Chief.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
